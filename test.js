9f06141b66e4aa175054816968f61069c0b3433f
PABLO A. LOPEZ DOMOWICZ <palopezd@us.ibm.com>
2019-08-28 12:58:16 -0400

    adding config div

--- test.js
+++ test.js
@@ -2,15 +2,17 @@ let head;
 document.addEventListener("DOMContentLoaded", (event) => {
   head = document.querySelector('.page-headline')
   if (head && head.innerText ==  "Pipeline yeet") {
-    buildButton()
+    buildUiElements()
   }
 });
 
-let buildButton = () => {
+let buildUiElements = () => {
   console.log('building');
+  let uiDiv = document.createElement('div');
+  uiDiv.className = 'submit-button primary'
   let newButton = document.createElement('button');
-  newButton.innerText = 'yeettown'
-  newButton.className = 'submit-button primary'
-  head.appendChild(newButton);
+  newButton.innerText = 'fire tests'
 
+  uiDiv.appendChild(newButton);
+  head.appendChild(uiDiv);
 }
