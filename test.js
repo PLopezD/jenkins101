7500ca22471ad1ee8d9aa0c76dd9775e41aab7ee
PABLO A. LOPEZ DOMOWICZ <palopezd@us.ibm.com>
2019-08-28 13:04:11 -0400

    formsubmit

--- test.js
+++ test.js
@@ -3,16 +3,28 @@ document.addEventListener("DOMContentLoaded", (event) => {
   head = document.querySelector('.page-headline')
   if (head && head.innerText ==  "Pipeline yeet") {
     buildUiElements()
+    addEventListenersToUi()
   }
 });
 
 let buildUiElements = () => {
   console.log('building');
   let uiDiv = document.createElement('div');
-  uiDiv.className = 'submit-button primary'
+
+  let form = document.createElement('form');
+  form.className = 'submit-button primary yui-button'
+  form.onSubmit = submitForm
+
   let newButton = document.createElement('button');
   newButton.innerText = 'fire tests'
+  newButton.id = 'testFire'
+  newButton.type = 'submit'
 
-  uiDiv.appendChild(newButton);
+  form.appendChild(newButton);
+  uiDiv.appendChild(form);
   head.appendChild(uiDiv);
+}
+
+let submitForm = () => {
+  console.log(1234);
 }
