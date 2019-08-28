6af646186e4d8fc94353606022500999c120493d
PABLO A. LOPEZ DOMOWICZ <palopezd@us.ibm.com>
2019-08-28 10:22:59 -0400

    test

--- test.js
+++ test.js
@@ -1,1 +1,12 @@
-console.log(234)
+document.addEventListener("DOMContentLoaded", (event) => {
+  console.log( "ready!" );
+  let head = document.querySelector('.page-headline')
+  if (head && head.innerText ==  "Pipeline yeet") {
+    console.log('Kickoff')
+    buildButton()
+  }
+});
+
+let buildButton = () => {
+  console.log('building');
+}
